58e4aefbbf7b6a79febb920fdc43a7e57d953120
Trevalyan <etstevens60@gmail.com>
2021-07-17 16:25:39 +0100

    [Day 1] Added two more test scenarios

--- tests/unit_tests/advent/test_entry.py
+++ tests/unit_tests/advent/test_entry.py
@@ -20,3 +20,19 @@ class TestEntry(unittest.TestCase):
 
         expected_fuel = 2
         self.assertEqual(expected_fuel, fuel)
+
+    def test_scenario_three(self):
+        example_mass = 1969
+
+        fuel = _calculate_fuel(example_mass)
+
+        expected_fuel = 654
+        self.assertEqual(expected_fuel, fuel)
+
+    def test_scenario_four(self):
+        example_mass = 100756
+
+        fuel = _calculate_fuel(example_mass)
+
+        expected_fuel = 33583
+        self.assertEqual(expected_fuel, fuel)
