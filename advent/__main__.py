2d2ed7b60562f32493cfcd2f0e8889d975ec5ae0
Trevalyan <etstevens60@gmail.com>
2021-07-17 15:54:18 +0100

    [Day 1] Initial solution

---
+++ advent/__main__.py
@@ -0,0 +1,16 @@
+import math
+
+
+def run():
+    masses = [12, 14]
+    for mass in masses:
+        fuel = _calculate_fuel(mass)
+        print(f"For {mass} mass, {fuel} fuel is required")
+
+
+def _calculate_fuel(mass):
+    return math.floor(mass / 3) - 2
+
+
+if __name__ == "__main__":
+    run()
