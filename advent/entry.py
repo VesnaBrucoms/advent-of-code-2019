401f25983ad04c919bb4e88f62b875938c8a8f66
Trevalyan <etstevens60@gmail.com>
2021-07-17 17:37:01 +0100

    [Day 1] Calculating fuel for the fuel

--- advent/entry.py
+++ advent/entry.py
@@ -2,11 +2,39 @@ import math
 
 
 def run():
-    masses = [12, 14]
+    masses = _read_file('day_1.txt')
+    fuel_total = 0
     for mass in masses:
-        fuel = _calculate_fuel(mass)
-        print(f'For {mass} mass, {fuel} fuel is required')
+        calculated_fuel = _calculate_fuel_for_mass(mass)
+        calculated_fuel += _calculate_fuel_for_fuel(calculated_fuel)
+        fuel_total += calculated_fuel
+    print(fuel_total)
 
 
-def _calculate_fuel(mass):
-    return math.floor(mass / 3) - 2
+def _read_file(filename):
+    inputs = []
+    with open(f'./tests/puzzle_inputs/{filename}', 'r') as puzzle_input:
+        for line in puzzle_input:
+            inputs.append(int(line))
+    return inputs
+
+
+def _calculate_fuel_for_fuel(fuel):
+    running_total = 0
+    calculated_fuel = _calculate_fuel_for_mass(fuel)
+    if calculated_fuel > 0:
+        running_total += _calculate_fuel_for_fuel(calculated_fuel)
+
+    running_total += calculated_fuel
+    return running_total
+
+
+def _calculate_fuel_for_mass(mass):
+    fuel = math.floor(mass / 3) - 2
+    if fuel < 0:
+        return 0
+    return fuel
+
+
+if __name__ == '__main__':
+    run()
--- tests/unit_tests/advent/test_entry.py
+++ tests/unit_tests/advent/test_entry.py
@@ -1,38 +1,63 @@
 import unittest
 
-from advent.entry import _calculate_fuel
+from advent.entry import _calculate_fuel_for_fuel
+from advent.entry import _calculate_fuel_for_mass
 
 
 class TestEntry(unittest.TestCase):
 
-    def test_scenario_one(self):
+    def test_calculate_fuel_for_mass_scenario_one(self):
         example_mass = 12
 
-        fuel = _calculate_fuel(example_mass)
+        fuel = _calculate_fuel_for_mass(example_mass)
 
         expected_fuel = 2
         self.assertEqual(expected_fuel, fuel)
 
-    def test_scenario_two(self):
+    def test_calculate_fuel_for_mass_scenario_two(self):
         example_mass = 14
 
-        fuel = _calculate_fuel(example_mass)
+        fuel = _calculate_fuel_for_mass(example_mass)
 
         expected_fuel = 2
         self.assertEqual(expected_fuel, fuel)
 
-    def test_scenario_three(self):
+    def test_calculate_fuel_for_mass_scenario_three(self):
         example_mass = 1969
 
-        fuel = _calculate_fuel(example_mass)
+        fuel = _calculate_fuel_for_mass(example_mass)
 
         expected_fuel = 654
         self.assertEqual(expected_fuel, fuel)
 
-    def test_scenario_four(self):
+    def test_calculate_fuel_for_mass_scenario_four(self):
         example_mass = 100756
 
-        fuel = _calculate_fuel(example_mass)
+        fuel = _calculate_fuel_for_mass(example_mass)
 
         expected_fuel = 33583
+        self.assertEqual(expected_fuel, fuel)
+
+    def test_calculate_fuel_for_mass_scenario_five(self):
+        example_mass = 2
+
+        fuel = _calculate_fuel_for_mass(example_mass)
+
+        expected_fuel = 0
+        self.assertEqual(expected_fuel, fuel)
+
+    def test_calculate_fuel_for_fuel_scenario_one(self):
+        example_mass = 654
+
+        fuel = _calculate_fuel_for_fuel(example_mass)
+
+        expected_fuel = 312
+        self.assertEqual(expected_fuel, fuel)
+
+    def test_calculate_fuel_for_fuel_scenario_two(self):
+        example_mass = 33583
+
+        fuel = _calculate_fuel_for_fuel(example_mass)
+
+        expected_fuel = 16763
         self.assertEqual(expected_fuel, fuel)
