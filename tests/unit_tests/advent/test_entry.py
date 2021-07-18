fe53437a7a5ed7cd3217ea664f288ed94df37b32
Trevalyan <etstevens60@gmail.com>
2021-07-18 12:23:24 +0100

    [Day 1] Parameterised tests
    * Added requirements.txt

--- tests/unit_tests/advent/test_entry.py
+++ tests/unit_tests/advent/test_entry.py
@@ -1,3 +1,4 @@
+from parameterized import parameterized
 import unittest
 
 from advent.entry import _calculate_fuel_for_fuel
@@ -6,58 +7,23 @@ from advent.entry import _calculate_fuel_for_mass
 
 class TestEntry(unittest.TestCase):
 
-    def test_calculate_fuel_for_mass_scenario_one(self):
-        example_mass = 12
+    @parameterized.expand([
+        ("simple_one", 12, 2),
+        ("simple_two", 14, 2),
+        ("typical_one", 1969, 654),
+        ("typical_two", 100756, 33583),
+        ("negative", 2, 0)
+    ])
+    def test_calculate_fuel_for_mass(self, name, given_mass, expected_fuel):
+        fuel = _calculate_fuel_for_mass(given_mass)
 
-        fuel = _calculate_fuel_for_mass(example_mass)
-
-        expected_fuel = 2
         self.assertEqual(expected_fuel, fuel)
 
-    def test_calculate_fuel_for_mass_scenario_two(self):
-        example_mass = 14
+    @parameterized.expand([
+        ("small", 654, 312),
+        ("large", 33583, 16763),
+    ])
+    def test_calculate_fuel_for_fuel(self, name, given_mass, expected_fuel):
+        fuel = _calculate_fuel_for_fuel(given_mass)
 
-        fuel = _calculate_fuel_for_mass(example_mass)
-
-        expected_fuel = 2
-        self.assertEqual(expected_fuel, fuel)
-
-    def test_calculate_fuel_for_mass_scenario_three(self):
-        example_mass = 1969
-
-        fuel = _calculate_fuel_for_mass(example_mass)
-
-        expected_fuel = 654
-        self.assertEqual(expected_fuel, fuel)
-
-    def test_calculate_fuel_for_mass_scenario_four(self):
-        example_mass = 100756
-
-        fuel = _calculate_fuel_for_mass(example_mass)
-
-        expected_fuel = 33583
-        self.assertEqual(expected_fuel, fuel)
-
-    def test_calculate_fuel_for_mass_scenario_five(self):
-        example_mass = 2
-
-        fuel = _calculate_fuel_for_mass(example_mass)
-
-        expected_fuel = 0
-        self.assertEqual(expected_fuel, fuel)
-
-    def test_calculate_fuel_for_fuel_scenario_one(self):
-        example_mass = 654
-
-        fuel = _calculate_fuel_for_fuel(example_mass)
-
-        expected_fuel = 312
-        self.assertEqual(expected_fuel, fuel)
-
-    def test_calculate_fuel_for_fuel_scenario_two(self):
-        example_mass = 33583
-
-        fuel = _calculate_fuel_for_fuel(example_mass)
-
-        expected_fuel = 16763
         self.assertEqual(expected_fuel, fuel)
